5f0e1abf4a72be04f98f21c7c2027694704f69ce
AlexsanderDamaceno <aemgbo@gmail.com>
2024-03-13 05:58:46 -0300

    solve some bugsin dependency

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,7 +5,7 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import {styled} from '@mui/styles';
+
 
 
 import { useNavigate } from 'react-router-dom';
